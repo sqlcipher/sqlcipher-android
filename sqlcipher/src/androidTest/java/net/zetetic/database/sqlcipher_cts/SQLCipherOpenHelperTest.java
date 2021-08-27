0abc8f324e453f2eac4cad0221fe4396b38d0213
Nick Parker <nparker@zetetic.net>
2021-08-27 16:49:24 -0500

    Adjust SQLCipherOpenHelper test

--- sqlcipher/src/androidTest/java/net/zetetic/database/sqlcipher_cts/SQLCipherOpenHelperTest.java
+++ sqlcipher/src/androidTest/java/net/zetetic/database/sqlcipher_cts/SQLCipherOpenHelperTest.java
@@ -5,9 +5,13 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import android.content.Context;
+import android.util.Log;
 
+import net.zetetic.database.sqlcipher.SQLiteConnection;
 import net.zetetic.database.sqlcipher.SQLiteDatabase;
+import net.zetetic.database.sqlcipher.SQLiteDatabaseHook;
 import net.zetetic.database.sqlcipher.SQLiteOpenHelper;
+import net.zetetic.database.sqlcipher.SQLiteStatement;
 
 import org.junit.Test;
 
@@ -16,20 +20,35 @@ public class SQLCipherOpenHelperTest extends AndroidSQLCipherTestCase {
   @Test
   public void shouldAccessReadOnlyDatabaseFromOpenHelper(){
     database.close();
-    DatabaseHelper helper = new DatabaseHelper(context);
+    SqlCipherOpenHelper helper = new SqlCipherOpenHelper(context);
     SQLiteDatabase db = helper.getReadableDatabase();
+    SQLiteStatement statement = db.compileStatement("PRAGMA cipher_provider_version;");
+    String version = statement.simpleQueryForString();
+    Log.i(TAG, String.format("SQLCipher provider version:%s", version));
     assertThat(db, is(notNullValue()));
   }
 
-  private class DatabaseHelper extends SQLiteOpenHelper {
+  private class SqlCipherOpenHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(Context context){
-      super(context, "foo.db", "foo", null, 1, 1, null, null);
+    private final String TAG = getClass().getSimpleName();
+
+    public SqlCipherOpenHelper(Context context) {
+      super(context, "test.db", "test", null, 1, 1, sqLiteDatabase -> Log.e(SQLCipherOpenHelperTest.this.TAG, "onCorruption()"), new SQLiteDatabaseHook() {
+        @Override
+        public void preKey(SQLiteConnection sqLiteConnection) {
+          Log.d(SQLCipherOpenHelperTest.this.TAG, "preKey()");
+        }
+
+        @Override
+        public void postKey(SQLiteConnection sqLiteConnection) {
+          Log.d(SQLCipherOpenHelperTest.this.TAG, "postKey()");
+        }
+      });
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+      db.execSQL("CREATE TABLE test (_id INTEGER PRIMARY KEY, value TEXT NOT NULL)");
     }
 
     @Override
