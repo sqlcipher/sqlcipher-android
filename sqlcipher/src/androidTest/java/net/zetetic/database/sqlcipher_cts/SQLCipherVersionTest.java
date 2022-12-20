9ce46d578b5a78663c685223556072a63552275b
Nick Parker <nparker@zetetic.net>
2022-12-20 11:44:44 -0600

    Update test for 4.5.3 release

--- sqlcipher/src/androidTest/java/net/zetetic/database/sqlcipher_cts/SQLCipherVersionTest.java
+++ sqlcipher/src/androidTest/java/net/zetetic/database/sqlcipher_cts/SQLCipherVersionTest.java
@@ -17,6 +17,6 @@ public class SQLCipherVersionTest extends AndroidSQLCipherTestCase {
       cipherVersion = cursor.getString(0);
       cursor.close();
     }
-    assertThat(cipherVersion, containsString("4.5.2"));
+    assertThat(cipherVersion, containsString("4.5.3"));
   }
 }
