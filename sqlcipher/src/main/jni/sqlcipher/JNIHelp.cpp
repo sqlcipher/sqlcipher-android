99b056998ab2468b89bce9a02fc150176b31346c
Nick Parker <nparker@zetetic.net>
2026-02-20 10:53:10 -0600

    Update min SDK to 23, update support library versions
    Support API updates:
    androidx.sqlite:sqlite updated to 2.6.2
    
    Testing API updates:
    androidx.room:room-common updated to 2.8.4
    androidx.room:room-runtime updated to 2.8.4
    androidx.room:room-compiler updated to 2.8.4

--- sqlcipher/src/main/jni/sqlcipher/JNIHelp.cpp
+++ sqlcipher/src/main/jni/sqlcipher/JNIHelp.cpp
@@ -294,13 +294,11 @@ const char* jniStrError(int errnum, char* buf, size_t buflen) {
     // char *strerror_r(int errnum, char *buf, size_t n);
     return strerror_r(errnum, buf, buflen);
 #else
-    int rc = strerror_r(errnum, buf, buflen);
-    if (rc != 0) {
-        // (POSIX only guarantees a value other than 0. The safest
-        // way to implement this function is to use C++ and overload on the
-        // type of strerror_r to accurately distinguish GNU from POSIX.)
-        snprintf(buf, buflen, "errno %d", errnum);
+    char* msg = strerror_r(errnum, buf, buflen);
+    if (msg != nullptr){
+        return msg;
     }
+    snprintf(buf, buflen, "errno %d", errnum);
     return buf;
 #endif
 }
